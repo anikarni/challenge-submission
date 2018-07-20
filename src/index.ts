447cbe000dd2f729c9cd5f715eb98a52aae8d675
Anike Arni <anikarni@gmail.com>
2018-07-19 22:03:36 -0400

    adds templating

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import path from 'path'
 import express from 'express'
+
 const app = express()
 
 const PORT = 8080
 
-app.get('/', (req, res) => res.send('Hello'))
+app.set('views', path.join(__dirname, '../templates'))
+app.set('view engine', 'ejs')
+
+// Routing
+app.get('/', (req, res) => res.render('index'))
+
+// Start app
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
